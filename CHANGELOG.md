b959a70faf2339fe829348ccd6ff5a358a54a92c
Kevin Lamontagne <kevin@demarque.com>
2014-05-09 10:34:08 -0400

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of mod_qos
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'mod_qos'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures mod_qos'
+long_description 'Installs/Configures mod_qos'
+version          '0.1.0'
+
